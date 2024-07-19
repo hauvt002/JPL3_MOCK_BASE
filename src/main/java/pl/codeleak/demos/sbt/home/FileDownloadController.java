10676c431462c7083af03833f8a64fc5690b03f7
hauhanh1998 <hauhanh1998@gmail.com>
2024-07-19 15:23:56 +0700

    abc

--- src/main/java/pl/codeleak/demos/sbt/home/FileDownloadController.java
+++ src/main/java/pl/codeleak/demos/sbt/home/FileDownloadController.java
@@ -7,16 +7,18 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pl.codeleak.demos.sbt.service.FileStorageService;
 
+import javax.persistence.Column;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 
-@RestController
+@Controller
 public class FileDownloadController {
     private static final Logger logger = (Logger) LoggerFactory.getLogger(FileDownloadController.class);
 
--- src/main/java/pl/codeleak/demos/sbt/home/FileUploadController.java
+++ src/main/java/pl/codeleak/demos/sbt/home/FileUploadController.java
@@ -2,9 +2,8 @@ package pl.codeleak.demos.sbt.home;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import pl.codeleak.demos.sbt.dto.response.FileResponseDTO;
@@ -14,11 +13,38 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RestController
-
+@Controller
+@RequestMapping("/files/")
 public class FileUploadController {
     @Autowired
     private FileStorageService fileStorageService;
+
+    @GetMapping("/uploadFile")
+    public String loadUpLoadFile(Model model){
+        model.addAttribute("message", null);
+        return "th-upload-form";
+    }
+
+
+    @PostMapping("/uploadFilePF")
+    public String uploadFile(Model model,@RequestParam("file") MultipartFile file) {
+        String message = "";
+        try{
+            String fileName = fileStorageService.storeFile(file);
+            String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                    .path("/downloadFile/")
+                    .path(fileName).toUriString();
+            message = "Uploaded the file successfully: " + file.getOriginalFilename();
+            model.addAttribute("message", message);
+        }catch (Exception e){
+            message = "Could not upload the file: " + file.getOriginalFilename() + ". Error: " +
+                    e.getMessage();
+            model.addAttribute("message", message);
+        }
+        return "th-upload-form";
+    }
+
+
     @PostMapping("/uploadFile")
     public FileResponseDTO uploadFile(@RequestParam("file") MultipartFile file) {
         String fileName = fileStorageService.storeFile(file);
